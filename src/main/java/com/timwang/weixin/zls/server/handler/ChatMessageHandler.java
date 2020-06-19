f7003bd70ce002f71a547b18dd61162e9c8510ba
wj89757 <961214034@qq.com>
2020-06-19 22:55:23 +0800

    init weixin java from weixin4j

---
+++ src/main/java/com/timwang/weixin/zls/server/handler/ChatMessageHandler.java
@@ -0,0 +1,26 @@
+package com.timwang.weixin.zls.server.handler;
+
+import java.util.Set;
+
+import com.foxinmy.weixin4j.handler.WeixinMessageHandler;
+import com.foxinmy.weixin4j.qy.chat.WeixinChatMessage;
+import com.foxinmy.weixin4j.request.WeixinMessage;
+import com.foxinmy.weixin4j.request.WeixinRequest;
+import com.foxinmy.weixin4j.response.BlankResponse;
+import com.foxinmy.weixin4j.response.WeixinResponse;
+
+public class ChatMessageHandler implements WeixinMessageHandler {
+
+    public boolean canHandle(WeixinRequest request, WeixinMessage message, Set<String> nodeNames) {
+        return nodeNames.contains("PackageId");
+    }
+
+    public WeixinResponse doHandle(WeixinRequest request, WeixinMessage message) {
+        WeixinChatMessage chatMessage = null; // 转换为实体
+        return BlankResponse.global;
+    }
+
+    public int weight() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/timwang/weixin/zls/server/handler/TextMessageHandler.java
@@ -0,0 +1,25 @@
+package com.timwang.weixin.zls.server.handler;
+
+import org.springframework.stereotype.Component;
+
+import com.foxinmy.weixin4j.handler.MessageHandlerAdapter;
+import com.foxinmy.weixin4j.message.TextMessage;
+import com.foxinmy.weixin4j.request.WeixinRequest;
+import com.foxinmy.weixin4j.response.TextResponse;
+import com.foxinmy.weixin4j.response.WeixinResponse;
+
+/**
+ * 文本消息处理
+ *
+ * @className TextMessageHandler
+ * @author jinyu(foxinmy@gmail.com)
+ * @date 2015年11月18日
+ * @since JDK 1.6
+ */
+@Component
+public class TextMessageHandler extends MessageHandlerAdapter<TextMessage> {
+    @Override
+    public WeixinResponse doHandle0(TextMessage message) {
+        return new TextResponse("收到了文本消息");
+    }
+}
